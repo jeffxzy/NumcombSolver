f3e52298d50e409352a00af863915a956bcd189a
jeffxzy <xuzhengyangjeff@126.com>
2023-03-07 22:54:01 +0800

    improve: expect score to 189 (avescore = 189.14, 51000 tests)

--- README.md
+++ README.md
@@ -4,4 +4,16 @@ A solver for game numcomb
 ## Tutorial
 1. clone
 2. cd into V7-185 or W2-181 (V7 has a higher average score, W2 is more readable)
-2. python run.py
+3. python run.py
+
+
+
++ test: Perform several simulation runs and obtain the average score.
++ play: Input the blocks one by one, and the program will automatically suggest and fill in the recommended position.
++ eval: Input a certain situation, and then input the blocks one by one. The program will suggest the recommended position, but you can manually fill in a different position.
++ train: Attempt to fine-tune the parameters through the minimize function.
++ bf: Attempt to fine-tune the parameters through brute force.
+
+The input format for blocks is "456" or "4 5 6".
+
+In particular, the input method for the wildcard block is "101010" or "10 10 10".
--- V7-185/run.py
+++ V7-185/run.py
@@ -24,8 +24,8 @@ def main():
         type = input("input: train / bf / test / eval / play / quit\n")
 
         # x0 = np.array([1.00, 0.65, 0.30, 0.10, 0.07, 0.03])
-        x0 = np.array([1.00, 0.721, 0.363, 0.177, 0.069, 0.024,
-                       0.50, 0.03, 0.01, 0.1])
+        x0 = np.array([1.00, 0.721, 0.3993, 0.1947, 0.069, 0.0312,
+                       0.75, 0.03, 0.01, 0.1])
         # x0: [1, 0.66068835, 0.30203273, 0.100673, 0.07100848, 0.03095212]
 
         if type == 'train':
@@ -47,21 +47,21 @@ def main():
                 return
             best, bestx0 = 0, []
             for item in range(0, 100):
-                for times in range(2, 4):
-                    for i in range(10, 11):
+                for times in range(1, 3):
+                    for i in range(6, 10):
                         best = 0
                         now = x0[i]
                         step = now
                         for j in range(0, times):
                             step = step / 10
-                        for j in range(-5, 6):
+                        for j in range(-2, 3):
                             x0[i] = now + j * step
                             score = f(x0, tt)
                             # print(score, ' --> ', x0)
                             if score > best:
                                 best, bestx0 = score, copy.deepcopy(x0)
                                 # print('Update New Best!')
-                            # print('?',best,bestx0)
+                            # print(x0)
                         print('\nOK remember best update:',best, ' --> ', bestx0)
                         x0 = copy.deepcopy(bestx0)
 
